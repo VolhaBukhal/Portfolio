531fd5ad5e5829a7e6704b8f88f5621bdeb58e03
Volha <o.bukhal@gmail.com>
2021-05-08 01:09:12 +0300

    promo section without sidepanel

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "./src"
+            baseDir: "./"
         }
     });
 
@@ -29,40 +29,40 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
-    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
-    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+    gulp.watch("./src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("./src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("./src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("./src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("./src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("./src/img/**/*").on('all', gulp.parallel('images'));
 });
 
 gulp.task('html', function () {
-    return gulp.src("src/*.html")
+    return gulp.src(".src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
 });
 
 gulp.task('scripts', function () {
-    return gulp.src("src/js/**/*.js")
+    return gulp.src("./src/js/**/*.js")
         .pipe(gulp.dest("dist/js"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function () {
-    return gulp.src("src/fonts/**/*")
+    return gulp.src("./src/fonts/**/*")
         .pipe(gulp.dest("dist/fonts"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('icons', function () {
-    return gulp.src("src/icons/**/*")
+    return gulp.src("./src/icons/**/*")
         .pipe(gulp.dest("dist/icons"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('images', function () {
-    return gulp.src("src/img/**/*")
+    return gulp.src("./src/img/**/*")
         .pipe(imagemin())
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
