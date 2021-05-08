87c47238252efa247a55ae50b3e48c46575adbec
Volha <o.bukhal@gmail.com>
2021-05-09 01:38:10 +0300

    about me section without mixin circle

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const burger = document.querySelector('.burger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+      
+burger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
