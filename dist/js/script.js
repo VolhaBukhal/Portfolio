989b6b749d1190b6deb495388dd30e954781a656
Volha <o.bukhal@gmail.com>
2021-05-11 16:25:10 +0300

    experience section done

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,14 @@ burger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+// автоматически присваиваем значения в стили .skill-item__progress_part
+// нужно добавить проверку на ввод данный, на буквы, и значение цифры
+const percents = document.querySelectorAll('.skill-item__progress');
+const lines = document.querySelectorAll('.skill-item__progress_part');
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML; 
+});
+
+
